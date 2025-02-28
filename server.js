bc0c3d0e354297c0f1c696619535109b9c49afa5
Daniel <danieldiazmc@gmail.com>
2025-02-28 01:20:12 -0400

    add env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 
 const app = express();
 const PORT = 9000;
-const SECRET = "5ZhiX{>x2T62"; // Mismo secret configurado en GitHub
+const SECRET = process.env.WEBHOOK_SECRET; // Mismo secret configurado en GitHub
 
 app.use(express.json());
 
